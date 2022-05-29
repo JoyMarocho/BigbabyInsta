df5b494d4719231dd76c3c7f636251d4b62ea9ba
JoyMarocho <joymarocho@gmail.com>
2022-05-30 01:53:46 +0300

    connect logout view function to its specific URL

--- bigbabyimages/urls.py
+++ bigbabyimages/urls.py
@@ -3,6 +3,10 @@ from django.conf import settings
 
 
 urlpatterns = [
-  #url(r'^$', views.index, name='index'),
-  url(r'register', views.register_new_user, name='register'),
-  url(r'login', views.login_user, name='login'),
+    #url(r'^$', views.index, name='index'),
+    url(r'register', views.register_new_user, name='register'),
+    url(r'login', views.login_user, name='login'),
+    url(r'logout', views.logout_user, name='logout'),
+
+    
+]
