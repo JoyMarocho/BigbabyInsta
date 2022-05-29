e38bc8ba3f5158d21d4ec44c2060e557ab21d0a0
JoyMarocho <joymarocho@gmail.com>
2022-05-30 01:36:46 +0300

    connect my view function to a specific URL

--- bigbabyimages/urls.py
+++ bigbabyimages/urls.py
@@ -1,38 +1,8 @@
+from django.conf.urls import url, include
+from django.conf import settings
 
 
-
-
-    # Create your views here.
-@login_required(login_url='/login')
-def index(request):
-    images = Image.objects.all()
-    # comments = image.comments.filter(active=True)
-    # new_comment = None
-    # if request.method == 'POST':
-    #   comment_form = CommentForm(data=request.POST)
-    #   if comment_form.is_valid():
-
-    #     #Create Comment object but fail to save to the database yet
-    #     new_comment = comment_form.save(commit=False)
-
-    #     #Assign current post to comment
-    #     new_comment.image = image
-
-    #     #Save comment to database
-    #     new_comment.save()
-    #   else:
-    #     comment_form = CommentForm
-    return render(request, 'index.html', {"images":images})
-
-def register_new_user(request):
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Registration Successful.')
-            return redirect('index')
-        messages.error(request, 'Unsuccessful registration. Invalid information.')
-    form = NewUserForm()
-    return render(request, 'registration/registration_form.html', {"registration_form": form} )
-
+urlpatterns = [
+  #url(r'^$', views.index, name='index'),
+  url(r'register', views.register_new_user, name='register'),
+  url(r'login', views.login_user, name='login'),
