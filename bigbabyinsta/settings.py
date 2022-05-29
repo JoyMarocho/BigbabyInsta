ffd97d414b34605b5415591ca809ba7d116dde02
JoyMarocho <joymarocho@gmail.com>
2022-05-30 02:53:45 +0300

    creating templates

--- bigbabyinsta/settings.py
+++ bigbabyinsta/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'bigbabyimages.apps.BigbabyimagesConfig',
+    'cloudinary',
+    'bootstrap5',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
