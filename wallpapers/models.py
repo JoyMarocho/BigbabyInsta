30907ffd51c2b16c1ffe9423272189ac68db2cb4
JoyMarocho <joymarocho@gmail.com>
2022-05-30 06:15:59 +0300

    creating a photo model

--- wallpapers/models.py
+++ wallpapers/models.py
@@ -1,34 +1,21 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
-from django.contrib.auth.models import User
-from django.urls import reverse
 from django.utils import timezone
-
+from location_field.models.plain import PlainLocationField
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class Image(models.Model):
-    name = models.CharField(max_length=35)
-    caption = models.TextField(default='caption')
-    photo = CloudinaryField('image',default='photo.jpeg')
-    username = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
-    posted_on = models.DateTimeField(default=timezone.now)
+class Photo(models.Model):
+    photo_name = models.CharField(max_length = 255)
+    description = models.TextField(default=1)
+    image = CloudinaryField('image', default='/media/default.jpg')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, default=1)
+    city = models.ForeignKey('City', on_delete=models.CASCADE,default=1)
 
+    def __str__(self):
+        return self.photo_name
 
-class Meta:
-    ordering = ['posted_on']
+    def save_photo(self):
+        self.save()
 
-def __str__(self):
-    return self.name
-
-def save_image(self):
-    self.save()
-
-def delete_image(self):
-    self.delete()
-
-def get_absolute_url(self):
-    return reverse('image-detail')
-
-# def update_image(self):
-#   fetched_object = Image.objects.filter(author=current_value).update(author=new_value)
-#   return fetched_object
+    def delete_photo(self):
+        self.delete()
